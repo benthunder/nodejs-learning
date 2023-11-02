813b45b0f00f2b0741772a9d83d2bea1dc32c891
Lê Đình Thắng <ledinhthang102@gmail.com>
2023-11-02 03:08:30 +0000

    Init project

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+module.exports = app;
